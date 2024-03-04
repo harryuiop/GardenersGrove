73b5b70f39a4512960f99b2c891599e42ddd0e9e
Alexandra Belcher <abe150@uclive.ac.nz>
2024-03-05 10:06:32 +1300

    Added a view all gardens page with controller. Updated sidebar and css to access and better view the page.

---
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/ViewAllGardensController.java
@@ -0,0 +1,38 @@
+package nz.ac.canterbury.seng302.gardenersgrove.controller;
+
+import nz.ac.canterbury.seng302.gardenersgrove.service.GardenService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * This is a basic spring boot controller, note the @link{Controller} annotation which defines this.
+ * This controller defines endpoints as functions with specific HTTP mappings
+ */
+@Controller
+public class ViewAllGardensController {
+    Logger logger = LoggerFactory.getLogger(ViewAllGardensController.class);
+
+    private final GardenService gardenService;
+
+    @Autowired
+    public ViewAllGardensController(GardenService gardenService) {
+        this.gardenService = gardenService;
+    }
+
+    /**
+     * @return redirect to /view-all
+     */
+    @GetMapping("/view-all")
+    public String home(@RequestParam(name = "gardenId", required = false) Long gardenId, Model model) {
+        logger.info("GET /view-all");
+        model.addAttribute("gardens", gardenService.getAllGardens());
+        return "allGardens";
+    }
+
+
+}
