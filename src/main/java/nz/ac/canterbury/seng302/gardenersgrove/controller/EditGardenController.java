c21af2eebf42c7c33ed9c602ab0a61df7b69448f
Alexandra Belcher <abe150@uclive.ac.nz>
2024-03-05 17:44:30 +1300

    The edit page has been created and now edits the database. It does not meet all the special character checks yet.

---
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/EditGardenController.java
@@ -0,0 +1,69 @@
+package nz.ac.canterbury.seng302.gardenersgrove.controller;
+
+import nz.ac.canterbury.seng302.gardenersgrove.components.GardensSidebar;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
+import nz.ac.canterbury.seng302.gardenersgrove.service.GardenService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Controller for form example.
+ * Note the @link{Autowired} annotation giving us access to the @link{FormService} class automatically
+ */
+@Controller
+public class EditGardenController extends GardensSidebar {
+    Logger logger = LoggerFactory.getLogger(EditGardenController.class);
+
+    private final GardenService gardenService;
+
+    private Long id;
+
+    @Autowired
+    public EditGardenController(GardenService gardenService) {
+        this.gardenService = gardenService;
+        gardenService.saveGarden(new Garden("g1", "g", 1));
+    }
+
+    @PostMapping("/edit-garden")
+    public String submitForm(@RequestParam(name="gardenName") String gardenName,
+                             @RequestParam(name = "gardenLocation") String gardenLocation,
+                             @RequestParam(name = "gardenSize") float gardenSize,
+                             Model model) {
+        logger.info("POST /form");
+        logger.info("ID: " + this.id);
+        logger.info("Garden: " + gardenService.getGardenById(this.id).get());
+        if ( gardenService.getGardenById(this.id).isPresent()) {
+            Garden garden = gardenService.getGardenById(this.id).get();
+            garden.setName(gardenName);
+            garden.setLocation(gardenLocation);
+            garden.setSize(gardenSize);
+            gardenService.saveGarden(garden);
+        }
+        return "redirect:/view-garden?gardenId=" + this.id;
+    }
+
+    /**
+     * @param model (map-like) representation of results to be used by thymeleaf
+     * @return thymeleaf gardenResponse
+     */
+    @GetMapping("/edit-garden")
+    public String home(@RequestParam(name = "gardenId", required = true) Long gardenId, Model model) {
+        this.updateGardensSidebar(model, gardenService);
+        logger.info("GET /edit-garden");
+        this.id = gardenId;
+        if (gardenService.getGardenById(gardenId).isPresent()) {
+            Garden garden = gardenService.getGardenById(gardenId).get();
+            model.addAttribute("displayGardenName", garden.getName());
+            model.addAttribute("displayGardenLocation", garden.getLocation());
+            model.addAttribute("displayGardenSize", garden.getSize());
+        }
+        return "editGarden";
+    }
+}
