c9cdbac0514856b2badf2a909ef0a763e849ad22
Joshua Winter <jwi197@uclive.ac.nz>
2024-03-05 16:02:21 +1300

    Update garden form controller to have the correct valadation as in development branch. Send to view garden page on submit

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/GardenFormController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/GardenFormController.java
@@ -26,61 +26,80 @@ public class GardenFormController extends GardensSidebar {
     public GardenFormController(GardenService gardenService) {
         this.gardenService = gardenService;
     }
+
     /**
-     * Gets form to be displayed, includes the ability to display results of previous form when linked to from POST form
-     * @param displayGardenName previous name entered into form to be displayed
-     * @param displayGardenLocation previous favourite programming language entered into form to be displayed
-     * @param displayGardenSize
-     * @param model (map-like) representation of name, language and isJava boolean for use in thymeleaf
-     * @return thymeleaf gardenForm
+     * Gets form to be displayed, and passes previous form values to the HTML.
+     * @param model object that passes data through to the HTML.
+     * @return thymeleaf HTML gardenForm template.
      */
     @GetMapping("/form")
-    public String form(@RequestParam(name="displayGardenName", required = true, defaultValue = "") String displayGardenName,
-                       @RequestParam(name="displayGardenLocation", required = true, defaultValue = "") String displayGardenLocation,
-                       @RequestParam(name="displayGardenSize", required = false, defaultValue = "0") float displayGardenSize,
-                       Model model) {
+    public String form(Model model) {
         logger.info("GET /form");
         this.updateGardensSidebar(model, gardenService);
-        model.addAttribute("displayGardenName", displayGardenName);
-        model.addAttribute("displayGardenLocation", displayGardenLocation);
-        model.addAttribute("displayGardenSize", displayGardenSize);
-        model.addAttribute("Home", displayGardenLocation.equalsIgnoreCase("Home"));
+        model.addAttribute("gardenNameError", "");
+        model.addAttribute("gardenLocationError", "");
+        model.addAttribute("gardenSizeError", "");
         return "gardenForm";
     }
 
+    public boolean checkString(String string) {
+        return string.matches("[a-zA-Z0-9 .,\\-']*");
+    }
+
     /**
-     * Posts a form response with name and favourite language
-     * @param gardenName name if user
-     * @param gardenLocation users favourite programming language
-     * @param gardenSize
-     * @param model (map-like) representation of name, language and isJava boolean for use in thymeleaf,
-     *              with values being set to relevant parameters provided
-     * @return thymeleaf gardenForm
+     * Submits form and saves the garden to the database.
+     * @param gardenName The name of the garden as input by the user.
+     * @param gardenLocation The location of the garden as input by the user.
+     * @param gardenSize The size of the garden as input by the user.
+     * @param model object that passes data through to the HTML.
+     * @return thymeleaf HTML template to redirect to.
      */
     @PostMapping("/form")
-    public String submitForm( @RequestParam(name="gardenName") String gardenName,
-                              @RequestParam(name = "gardenLocation") String gardenLocation,
-                              @RequestParam(name = "gardenSize") float gardenSize,
-                              Model model) {
+    public String submitForm(@RequestParam(name = "gardenName") String gardenName,
+                             @RequestParam(name = "gardenLocation") String gardenLocation,
+                             @RequestParam(name = "gardenSize", required = false) Float gardenSize,
+                             Model model) {
         logger.info("POST /form");
-        gardenService.saveGarden(new Garden(gardenName, gardenLocation, gardenSize));
-        model.addAttribute("displayGardenName", gardenName);
-        model.addAttribute("displayGardenLocation", gardenLocation);
-        model.addAttribute("displayGardenSize", gardenSize);
-        model.addAttribute("Home", gardenLocation.equalsIgnoreCase("Home"));
-        this.updateGardensSidebar(model, gardenService);
-        return "gardenForm";
-    }
+        boolean nameIsValid = false;
+        boolean locationIsValid = false;
+        boolean sizeIsValid = false;
 
-    /**
-     * Gets all form responses
-     * @param model (map-like) representation of results to be used by thymeleaf
-     * @return thymeleaf gardenResponse
-     */
-    @GetMapping("/form/responses")
-    public String responses(Model model) {
-        logger.info("GET /form/responses");
-        model.addAttribute("responses", gardenService.getAllGardens());
-        return "gardenResponse";
+        if (gardenName.isBlank()) {
+            model.addAttribute("gardenNameError", "Garden name cannot by empty");
+        } else if (!checkString(gardenName)) {
+            model.addAttribute(
+                    "gardenNameError",
+                    "Garden name must only include letters, numbers, spaces, commas, dots, hyphens or apostrophes");
+        } else {
+            nameIsValid = true;
+        }
+
+        if (gardenLocation.isBlank()) {
+            model.addAttribute("gardenLocationError", "Location cannot be empty");
+        } else if (!checkString(gardenLocation)) {
+            model.addAttribute(
+                    "gardenLocationError",
+                    "Location name must only include letters, numbers, spaces, commas, dots, hyphens or apostrophes"
+            );
+        } else {
+            locationIsValid = true;
+        }
+
+        if (gardenSize != null && gardenSize <= 0) {
+            model.addAttribute("gardenSizeError", "Garden size must be a positive number");
+        } else {
+            sizeIsValid = true;
+        }
+
+        if (nameIsValid && locationIsValid && sizeIsValid) {
+            Garden garden = new Garden(gardenName, gardenLocation, gardenSize);
+            gardenService.saveGarden(garden);
+            return "redirect:/view-garden?gardenId=" + garden.getId();
+        } else {
+            model.addAttribute("gardenName", gardenName);
+            model.addAttribute("gardenLocation", gardenLocation);
+            model.addAttribute("gardenSize", gardenSize);
+            return "gardenForm";
+        }
     }
 }
