c01dac22ab8e5e5222f86a5b5fee661b835d13a6
Harrison Parkes <hpa101@uclive.ac.nz>
2024-03-05 17:00:54 +1300

    removed unnecessary debug log

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/GardenFormController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/GardenFormController.java
@@ -94,7 +94,6 @@ public class GardenFormController extends GardensSidebar {
         if (nameIsValid && locationIsValid && sizeIsValid) {
             Garden garden = new Garden(gardenName, gardenLocation, gardenSize);
             gardenService.saveGarden(garden);
-            logger.info("Garden Size: " + garden.getSize());
             return "redirect:/view-garden?gardenId=" + garden.getId();
         } else {
             model.addAttribute("gardenName", gardenName);
