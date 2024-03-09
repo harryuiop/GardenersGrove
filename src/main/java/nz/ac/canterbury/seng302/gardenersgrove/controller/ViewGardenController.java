b801b04100cafda4b3cd89545651410aafec9598
Joshua Winter <jwi197@uclive.ac.nz>
2024-03-09 21:01:24 +1300

    Add plus symbol for images and work on being able to change the image across multiple windows

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/ViewGardenController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/ViewGardenController.java
@@ -1,5 +1,6 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
+import nz.ac.canterbury.seng302.gardenersgrove.controller.validation.ImageValidation;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
 import nz.ac.canterbury.seng302.gardenersgrove.components.GardensSidebar;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Plant;
@@ -11,12 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.List;
-import java.util.Optional;
+import java.io.IOException;
+import java.util.*;
 
 /**
  * This is a basic spring boot controller, note the @link{Controller} annotation which defines this.
@@ -42,6 +43,7 @@ public class ViewGardenController extends GardensSidebar {
     public String home(@RequestParam(name = "gardenId", required = false) Long gardenId, Model model) {
         logger.info("GET /view-garden");
         this.updateGardensSidebar(model, gardenService);
+        model.addAttribute("gardenId", gardenId);
         model.addAttribute("garden", gardenService.getGardenById(gardenId));
 
         Optional<Garden> optionalGarden = gardenService.getGardenById(gardenId);
@@ -50,6 +52,26 @@ public class ViewGardenController extends GardensSidebar {
             List<Plant> plants = garden.getPlants();
             model.addAttribute("plants", plants);
         }
+        return "viewGarden";
+    }
+
+    @PostMapping("/view-garden")
+    public String submitForm(@RequestParam(name = "plantImage", required=false) MultipartFile imageFile
+            , Model model) throws IOException {
+        logger.info("POST/ plant image");
+
+        // The following code is temporary
+        this.updateGardensSidebar(model, gardenService);
+        model.addAttribute("gardenId", 1);
+        model.addAttribute("garden", gardenService.getGardenById(1l));
+
+        Optional<Garden> optionalGarden = gardenService.getGardenById(1l);
+        if (optionalGarden.isPresent()) {
+            Garden garden = optionalGarden.get();
+            List<Plant> plants = garden.getPlants();
+            model.addAttribute("plants", plants);
+        }
+
         return "viewGarden";
     }
 
