f597aea39240e2ee932512c79e826a6210699ecb
Harrison Parkes <hpa101@uclive.ac.nz>
2024-03-13 15:52:10 +1300

    updated image validation tests

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unit/ImageValidationTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unit/ImageValidationTest.java
@@ -1,55 +1,56 @@
 package nz.ac.canterbury.seng302.gardenersgrove.unit;
 
-import nz.ac.canterbury.seng302.gardenersgrove.controller.validation.ImageValidation;
-import org.junit.jupiter.api.BeforeAll;
+import nz.ac.canterbury.seng302.gardenersgrove.controller.validation.ImageValidator;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.Arrays;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ImageValidationTest {
-    private static ImageValidation imageValidation;
+    private static ImageValidator imageValidator;
 
-    private final int BYTES_IN_KBS = 1024;
+    private final int BYTES_IN_MB = 1_000_000;
 
-    @BeforeAll
-    public static void setup() {
-        imageValidation = new ImageValidation();
-    }
+    private static MultipartFile mockPng = new MockMultipartFile("plant.png", "plant.png", "image/png", new byte[1]);
+    private static MultipartFile mockJpg = new MockMultipartFile("plant.jpg", "plant.jpg", "image/jpeg", new byte[10_000_001]);
+    private static MultipartFile mockSvg = new MockMultipartFile("plant.svg", "plant.svg", "image/svg", new byte[1]);
+    private static MultipartFile mockGif = new MockMultipartFile("plant.gif", "plant.gif", "image/gif", new byte[1]);
 
-    @ParameterizedTest
-    @ValueSource(strings = {"plant.png", "plant.svg", "plant.jpg"})
-    void testImageTypeValid(String filename) {
-        assertTrue(imageValidation.checkImageType(filename));
+    @Test
+    void validateImage_pngType_returnTrue() {
+        imageValidator = new ImageValidator(mockPng);
+        assertTrue(imageValidator.validImageType());
     }
 
     @Test
-    void testImageTypeInvalid() {
-        String filename = "plant.gif";
-        assertFalse(imageValidation.checkImageType(filename));
+    void validateImage_jpegType_returnTrue() {
+        imageValidator = new ImageValidator(mockJpg);
+        assertTrue(imageValidator.validImageType());
     }
 
     @Test
-    void testImageSizeValid() {
-        byte[] smallImage = fakeByteArray(1);
-        assertTrue(imageValidation.checkImageSize(smallImage));
+    void validateImage_svgType_returnTrue() {
+        imageValidator = new ImageValidator(mockSvg);
+        assertTrue(imageValidator.validImageType());
+    }
+
+    @Test
+    void validateImage_invalidType_returnFalse() {
+        imageValidator = new ImageValidator(mockGif);
+        assertFalse(imageValidator.validImageType());
+    }
+
+    @Test
+    void validateImage_validSize_returnTrue() {
+        imageValidator = new ImageValidator(mockPng);
+        assertTrue(imageValidator.validSize());
     }
 
     @Test
     void testLargeImageSizeInvalid() {
-        byte[] largeImage = fakeByteArray(11);
-        assertFalse(imageValidation.checkImageSize(largeImage));
+        ImageValidator imageValidator = new ImageValidator(mockJpg);
+        assertFalse(imageValidator.validSize());
     }
-
-    private byte[] fakeByteArray(int sizeInMB) {
-        int sizeInBytes = sizeInMB * BYTES_IN_KBS * BYTES_IN_KBS;
-        byte[] fakeByteArray = new byte[sizeInBytes];
-        Arrays.fill(fakeByteArray, (byte) 0);
-        return fakeByteArray;
-    }
-
 }
