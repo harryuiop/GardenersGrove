aa8b4c2524288f8c89fd323f95d2318f1567e387
Harry Parkes <harrydparkes@proton.me>
2024-03-27 21:16:17 +1300

    only running profile picture script once DOM has loaded

--- src/main/resources/static/javascript/profilePictureUpload.js
+++ src/main/resources/static/javascript/profilePictureUpload.js
@@ -1,7 +1,9 @@
-document.getElementById('profilePicture').addEventListener(
-    'click', () => document.getElementById('profilePictureInput').click()
-);
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('profilePicture').addEventListener(
+        'click', () => document.getElementById('profilePictureInput').click()
+    );
 
-document.getElementById('profilePictureInput').addEventListener(
-    'change', () => document.getElementById('uploadProfileImage').submit()
-);
+    document.getElementById('profilePictureInput').addEventListener(
+        'change', () => document.getElementById('uploadProfileImage').submit()
+    );
+});
