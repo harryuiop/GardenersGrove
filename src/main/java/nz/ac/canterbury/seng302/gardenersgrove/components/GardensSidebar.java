a5a66ba41929e3faac42f1f980c23990d30286fc
Harrison Parkes <hpa101@uclive.ac.nz>
2024-03-28 13:43:24 +1300

    Fixed references to old Users class from dev branch

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/components/GardensSidebar.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/components/GardensSidebar.java
@@ -1,6 +1,6 @@
 package nz.ac.canterbury.seng302.gardenersgrove.components;
 
-import nz.ac.canterbury.seng302.gardenersgrove.entity.Users;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.User;
 import nz.ac.canterbury.seng302.gardenersgrove.service.GardenService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.UserService;
 import org.springframework.security.core.Authentication;
@@ -20,7 +20,7 @@ public class GardensSidebar {
         model.addAttribute("gardens", gardenService.getAllGardens());
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         int currentPrincipalName = Integer.parseInt(auth.getName());
-        Users user = userService.getUserById(currentPrincipalName);
+        User user = userService.getUserById(currentPrincipalName);
         model.addAttribute("user", user);
     }
 }
