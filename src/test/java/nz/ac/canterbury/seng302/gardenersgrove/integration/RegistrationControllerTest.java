b4c35f2f6ca6729c5a611c72ae080cebb5dfade9
Alexandra Belcher <abe150@uclive.ac.nz>
2024-03-28 17:20:40 +1300

    Added Registration Controller Tests. Currently struggling to get the blank last name but no surname is true to work. It goes through the whole post function however it doesn't save to the repository

---
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/RegistrationControllerTest.java
@@ -0,0 +1,290 @@
+package nz.ac.canterbury.seng302.gardenersgrove.integration;
+
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Users;
+import nz.ac.canterbury.seng302.gardenersgrove.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class RegistrationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+    }
+    @Test
+    void submitForm_allValid_userSaved() throws Exception {
+        String firstName = "John";
+        String lastName = "Smith";
+        Boolean noSurname = false;
+        String email = "john@smith.co.nz";
+        String address = "1 place street";
+        String password = "Password100%";
+        String passwordConfirm = "Password100%";
+        String dateOfBirth = String.valueOf(LocalDate.now().minusYears(20));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/register")
+                        .param("email", email)
+                        .param("firstName", firstName)
+                        .param("lastName", lastName)
+                        .param("noSurname", String.valueOf(noSurname))
+                        .param("address", address)
+                        .param("password", password)
+                        .param("passwordConfirm", passwordConfirm)
+                        .param("dateOfBirth", dateOfBirth))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrlPattern("/profile*"));
+
+        List<Users> allUsers = userRepository.findAll();
+        assertEquals(1, allUsers.size());
+        Users user = allUsers.get(0);
+        assertEquals(firstName, user.getFname());
+        assertEquals(lastName, user.getLname());
+        assertEquals(email, user.getEmail());
+        assertEquals(address, user.getAddress());
+        assertEquals(password, user.getPassword());
+        assertEquals(dateOfBirth, user.getDob());
+    }
+
+    @Test
+    void submitForm_lastNameBlankNoSurnameTrue_userSaved() throws Exception {
+        String firstName = "John";
+        String lastName = "";
+        Boolean noSurname = true;
+        String email = "john@smith.co.nz";
+        String address = "1 place street";
+        String password = "Password100%";
+        String passwordConfirm = "Password100%";
+        String dateOfBirth = String.valueOf(LocalDate.now().minusYears(20));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/register")
+                        .param("email", email)
+                        .param("firstName", firstName)
+                        .param("lastName", lastName)
+                        .param("noSurname", String.valueOf(noSurname))
+                        .param("address", address)
+                        .param("password", password)
+                        .param("passwordConfirm", passwordConfirm)
+                        .param("dateOfBirth", dateOfBirth))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrlPattern("/profile*"));
+
+        List<Users> allUsers = userRepository.findAll();
+        assertEquals(1, allUsers.size());
+        Users user = allUsers.get(0);
+        assertEquals(firstName, user.getFname());
+        assertEquals(lastName, user.getLname());
+        assertEquals(email, user.getEmail());
+        assertEquals(address, user.getAddress());
+        assertEquals(password, user.getPassword());
+        assertEquals(dateOfBirth, user.getDob());
+    }
+
+    @Test
+    void submitForm_invalidFirstName_userNotSaved() throws Exception {
+        String firstName = "John ###";
+        String lastName = "Smith";
+        Boolean noSurname = false;
+        String email = "john@smith.co.nz";
+        String address = "1 place street";
+        String password = "Password100%";
+        String passwordConfirm = "Password100%";
+        String dateOfBirth = String.valueOf(LocalDate.now().minusYears(20));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/register")
+                        .param("email", email)
+                        .param("firstName", firstName)
+                        .param("lastName", lastName)
+                        .param("noSurname", String.valueOf(noSurname))
+                        .param("address", address)
+                        .param("password", password)
+                        .param("passwordConfirm", passwordConfirm)
+                        .param("dateOfBirth", dateOfBirth))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("register"));
+
+        List<Users> allUsers = userRepository.findAll();
+        assertTrue(allUsers.isEmpty());
+    }
+
+    @Test
+    void submitForm_blankLastNameNoSurnameFalse_userNotSaved() throws Exception {
+        String firstName = "John";
+        String lastName = "   ";
+        Boolean noSurname = false;
+        String email = "john@smith.co.nz";
+        String address = "1 place street";
+        String password = "Password100%";
+        String passwordConfirm = "Password100%";
+        String dateOfBirth = String.valueOf(LocalDate.now().minusYears(20));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/register")
+                        .param("email", email)
+                        .param("firstName", firstName)
+                        .param("lastName", lastName)
+                        .param("noSurname", String.valueOf(noSurname))
+                        .param("address", address)
+                        .param("password", password)
+                        .param("passwordConfirm", passwordConfirm)
+                        .param("dateOfBirth", dateOfBirth))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("register"));
+
+        List<Users> allUsers = userRepository.findAll();
+        assertTrue(allUsers.isEmpty());
+    }
+
+    @Test
+    void submitForm_invalidEmailFormat_userNotSaved() throws Exception {
+        String firstName = "John-William";
+        String lastName = "Smith";
+        Boolean noSurname = false;
+        String email = "@jws.co.nz";
+        String address = "1 place street";
+        String password = "Password100%";
+        String passwordConfirm = "Password100%";
+        String dateOfBirth = String.valueOf(LocalDate.now().minusYears(20));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/register")
+                        .param("email", email)
+                        .param("firstName", firstName)
+                        .param("lastName", lastName)
+                        .param("noSurname", String.valueOf(noSurname))
+                        .param("address", address)
+                        .param("password", password)
+                        .param("passwordConfirm", passwordConfirm)
+                        .param("dateOfBirth", dateOfBirth))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("register"));
+
+        List<Users> allUsers = userRepository.findAll();
+        assertTrue(allUsers.isEmpty());
+    }
+
+    @Test
+    void submitForm_tooYoung_userNotSaved() throws Exception {
+        String firstName = "John William";
+        String lastName = "Smith";
+        Boolean noSurname = false;
+        String email = "john@smith.co.nz";
+        String address = "1 place street";
+        String password = "Password100%";
+        String passwordConfirm = "Password100%";
+        String dateOfBirth = String.valueOf(LocalDate.now().minusYears(12));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/register")
+                        .param("email", email)
+                        .param("firstName", firstName)
+                        .param("lastName", lastName)
+                        .param("noSurname", String.valueOf(noSurname))
+                        .param("address", address)
+                        .param("password", password)
+                        .param("passwordConfirm", passwordConfirm)
+                        .param("dateOfBirth", dateOfBirth))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("register"));
+
+        List<Users> allUsers = userRepository.findAll();
+        assertTrue(allUsers.isEmpty());
+    }
+
+    @Test
+    void submitForm_tooOld_userNotSaved() throws Exception {
+        String firstName = "John";
+        String lastName = "Smith";
+        Boolean noSurname = false;
+        String email = "john@smith.co.nz";
+        String address = "1 place street";
+        String password = "Password100%";
+        String passwordConfirm = "Password100%";
+        String dateOfBirth = String.valueOf(LocalDate.now().minusYears(121));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/register")
+                        .param("email", email)
+                        .param("firstName", firstName)
+                        .param("lastName", lastName)
+                        .param("noSurname", String.valueOf(noSurname))
+                        .param("address", address)
+                        .param("password", password)
+                        .param("passwordConfirm", passwordConfirm)
+                        .param("dateOfBirth", dateOfBirth))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("register"));
+
+        List<Users> allUsers = userRepository.findAll();
+        assertTrue(allUsers.isEmpty());
+    }
+
+    @Test
+    void submitForm_invalidPassword_userNotSaved() throws Exception {
+        String firstName = "John";
+        String lastName = "Smith";
+        Boolean noSurname = false;
+        String email = "john@smith.co.nz";
+        String address = "1 place street";
+        String password = "password";
+        String passwordConfirm = "Password100%";
+        String dateOfBirth = String.valueOf(LocalDate.now().minusYears(20));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/register")
+                        .param("email", email)
+                        .param("firstName", firstName)
+                        .param("lastName", lastName)
+                        .param("noSurname", String.valueOf(noSurname))
+                        .param("address", address)
+                        .param("password", password)
+                        .param("passwordConfirm", passwordConfirm)
+                        .param("dateOfBirth", dateOfBirth))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("register"));
+
+        List<Users> allUsers = userRepository.findAll();
+        assertTrue(allUsers.isEmpty());
+    }
+
+    @Test
+    void submitForm_missMatchingPasswords_userNotSaved() throws Exception {
+        String firstName = "John";
+        String lastName = "Smith";
+        Boolean noSurname = false;
+        String email = "john@smith.co.nz";
+        String address = "1 place street";
+        String password = "Password100%";
+        String passwordConfirm = "password%";
+        String dateOfBirth = String.valueOf(LocalDate.now().minusYears(20));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/register")
+                        .param("email", email)
+                        .param("firstName", firstName)
+                        .param("lastName", lastName)
+                        .param("noSurname", String.valueOf(noSurname))
+                        .param("address", address)
+                        .param("password", password)
+                        .param("passwordConfirm", passwordConfirm)
+                        .param("dateOfBirth", dateOfBirth))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("register"));
+
+        List<Users> allUsers = userRepository.findAll();
+        assertTrue(allUsers.isEmpty());
+    }
+}
