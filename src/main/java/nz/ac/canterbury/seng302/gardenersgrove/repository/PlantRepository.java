5c348e5f0d9f848344ee61d516204d0e1dfed7c9
Harrison Parkes <hpa101@uclive.ac.nz>
2024-04-03 20:18:08 +1300

    Put the findAll List function in PlantRepository for now as its being used in tests

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/repository/PlantRepository.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/repository/PlantRepository.java
@@ -3,9 +3,13 @@ package nz.ac.canterbury.seng302.gardenersgrove.repository;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Plant;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.NonNull;
 
 import java.util.List;
 
 public interface PlantRepository extends CrudRepository<Plant, Long> {
+    @NonNull
+    List<Plant> findAll();
+
     List<Plant> findAllByGarden(Garden garden);
 }
