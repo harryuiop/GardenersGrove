14f4cbf282e9de46f521bf5cf244c194db201bab
Harrison Ellis <hel46@uclive.ac.nz>
2024-04-04 14:30:13 +1300

    working implementation

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/HomeController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/HomeController.java
@@ -1,17 +1,18 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
-import nz.ac.canterbury.seng302.gardenersgrove.entity.User;
 import nz.ac.canterbury.seng302.gardenersgrove.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import static java.lang.Integer.parseInt;
-
+@Controller
 public class HomeController {
 
     private final UserService userService;
@@ -26,6 +27,7 @@ public class HomeController {
      * @param userService           The UserService responsible for user-related operations.
      * @param authenticationManager The AuthenticationManager for managing user authentication.
      */
+    @Autowired
     public HomeController(UserService userService, AuthenticationManager authenticationManager) {
         this.userService = userService;
         this.authenticationManager = authenticationManager;
@@ -40,15 +42,17 @@ public class HomeController {
     @GetMapping("/")
     public String getLandingPage(Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        boolean loggedIn;
-        if (auth != null && auth.isAuthenticated()) {
-            int currentPrincipalName = parseInt(auth.getName());
-            User user = userService.getUserById(currentPrincipalName);
-            loggedIn = user != null;
-        } else {
-            loggedIn = false;
+
+        // The home page is authorized to any user, logged in or not thus all users are technically authenticated,
+        // so we must check that the principle is linked to an actual user (which would return an int if true)
+        if (auth.getPrincipal() == "anonymousUser") {
+            boolean loggedIn = false;
+            model.addAttribute("loggedInStatus", loggedIn);
+            return "home";
         }
+
+        boolean loggedIn = true;
         model.addAttribute("loggedInStatus", loggedIn);
-        return "index";
+        return "home";
     }
 }
