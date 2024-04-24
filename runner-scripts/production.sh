2c0d646001c3825694efb52d541eed77860b521d
Harrison Ellis <hel46@uclive.ac.nz>
2024-04-24 17:46:52 +1200

    Update production.sh

--- runner-scripts/production.sh
+++ runner-scripts/production.sh
@@ -1,5 +1,7 @@
 fuser -k 10500/tcp || true
 
+source staging/.env
+
 java -jar production/libs/gardeners-grove-0.0.1-SNAPSHOT.jar \
     --server.port=10500 \
     --server.servlet.contextPath=/prod \
