c799c9f381d0b55452dc950e5f7c5baac2dbe4e6
Zoe Perry <zpe25@uclive.ac.nz>
2024-05-09 11:02:18 +1200

    Implement profanity checker

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/validation/FormValuesValidator.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/validation/FormValuesValidator.java
@@ -1,9 +1,17 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller.validation;
 
+import nz.ac.canterbury.seng302.gardenersgrove.controller.GardenController;
 import nz.ac.canterbury.seng302.gardenersgrove.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
+import java.util.Objects;
 
 /**
  * This class is used to provide static methods for validation of fields.
@@ -11,6 +19,46 @@ import java.time.format.DateTimeParseException;
 public class FormValuesValidator {
     // Matches letters, hyphens, apostrophes and spaces, with at least one character.
     static String namePattern = "^[a-zA-Z\\-' ]+$";
+    static Logger logger = LoggerFactory.getLogger(GardenController.class);
+
+    /**
+     * Checks if a given string contains inappropriate language
+     *
+     * @param string is the test that needs checking
+     * @return true if contains profanity, false otherwise
+     */
+    public static boolean checkProfanity(String string) {
+        try {
+            string = string.replace(" ", "%20");
+            URL url = new URL("https://www.purgomalum.com/service/containsprofanity?text=" + string);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setConnectTimeout(5000);
+            con.setReadTimeout(5000);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder content = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+            return Objects.equals(String.valueOf(content), "true");
+        } catch (Exception e) {
+            logger.info(e.toString());
+            return false;
+        }
+    }
+
+    /**
+     * Checks the String contains valid characters
+     *
+     * @param string represents the string being checked for correct characters
+     * @return whether it is valid or not.
+     */
+    public static boolean checkContainsText(String string) {
+        return string.isEmpty() || string.chars().anyMatch(Character::isAlphabetic);
+    }
 
     /**
      * Checks the String contains valid characters
