8671260c25fdaed86b7d1fdc6743fd6c10c2a46c
Harrison Parkes <hpa101@uclive.ac.nz>
2024-05-08 21:54:49 +1200

    created WeatherResponse class

---
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/weather/WeatherResponse.java
@@ -0,0 +1,108 @@
+package nz.ac.canterbury.seng302.gardenersgrove.weather;
+
+import java.util.List;
+
+public class WeatherResponse {
+    private double latitude;
+    private double longitude;
+    private double generationtime_ms;
+    private int utc_offset_seconds;
+    private String timezone;
+    private String timezone_abbreviation;
+    private double elevation;
+    private HourlyUnits hourly_units;
+    private Hourly hourly;
+
+    @Override
+    public String toString() {
+        return "WeatherResponse{" +
+                        "latitude=" + latitude +
+                        ", longitude=" + longitude +
+                        ", generationtime_ms=" + generationtime_ms +
+                        ", utc_offset_seconds=" + utc_offset_seconds +
+                        ", timezone='" + timezone + '\'' +
+                        ", timezone_abbreviation='" + timezone_abbreviation + '\'' +
+                        ", elevation=" + elevation +
+                        ", hourly_units=" + hourly_units +
+                        ", hourly=" + hourly +
+                        '}';
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setGenerationtime_ms(double generationtime_ms) {
+        this.generationtime_ms = generationtime_ms;
+    }
+
+    public void setUtc_offset_seconds(int utc_offset_seconds) {
+        this.utc_offset_seconds = utc_offset_seconds;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    public void setTimezone_abbreviation(String timezone_abbreviation) {
+        this.timezone_abbreviation = timezone_abbreviation;
+    }
+
+    public void setElevation(double elevation) {
+        this.elevation = elevation;
+    }
+
+    public void setHourly_units(HourlyUnits hourly_units) {
+        this.hourly_units = hourly_units;
+    }
+
+    public void setHourly(Hourly hourly) {
+        this.hourly = hourly;
+    }
+}
+
+class HourlyUnits {
+    private String time;
+    private String temperature_2m;
+
+    @Override
+    public String toString() {
+        return "HourlyUnits{" +
+                        "time='" + time + '\'' +
+                        ", temperature_2m='" + temperature_2m + '\'' +
+                        '}';
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setTemperature_2m(String temperature_2m) {
+        this.temperature_2m = temperature_2m;
+    }
+}
+
+class Hourly {
+    private List<String> time;
+    private List<Double> temperature_2m;
+
+    @Override
+    public String toString() {
+        return "Hourly{" +
+                        "time=" + time +
+                        ", temperature_2m=" + temperature_2m +
+                        '}';
+    }
+
+    public void setTime(List<String> time) {
+        this.time = time;
+    }
+
+    public void setTemperature_2m(List<Double> temperature_2m) {
+        this.temperature_2m = temperature_2m;
+    }
+}
