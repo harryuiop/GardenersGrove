25fef052bbe70b021717a84352ea9bfb71b64a7d
Harrison Parkes <hpa101@uclive.ac.nz>
2024-05-09 13:26:37 +1200

    correctly building weather api URI

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/service/WeatherService.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/service/WeatherService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nz.ac.canterbury.seng302.gardenersgrove.weather.WeatherResponse;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 import java.io.IOException;
 import java.net.URI;
@@ -12,18 +13,28 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class WeatherService {
-
     private final ObjectMapper objectMapper = new ObjectMapper()
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-    public String getJsonFromApi() throws InterruptedException {
+    public String getJsonFromApi(float latitude, float longitude) throws InterruptedException {
+        URI uri = new DefaultUriBuilderFactory().builder()
+                .scheme("https")
+                .host("api.open-meteo.com")
+                .path("v1/forecast")
+                .queryParam("latitude", latitude)
+                .queryParam("longitude", longitude)
+                .queryParam("hourly", "temperature_2m,weather_code")
+                .queryParam("timezone", "auto")
+                .queryParam("past_days", 2)
+                .queryParam("forecast_days", 3)
+                .build();
+
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"))
+                .uri(uri)
                 .GET()
                 .build();
         try (HttpClient client = HttpClient.newHttpClient()) {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            return response.body();
+            return client.send(request, HttpResponse.BodyHandlers.ofString()).body();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -32,7 +43,7 @@ public class WeatherService {
 
     public static void main(String[] args) throws InterruptedException {
         WeatherService weatherService = new WeatherService();
-        String json = weatherService.getJsonFromApi();
+        String json = weatherService.getJsonFromApi(-43.533f, 172.6333f);
         try {
             WeatherResponse weatherResponse = weatherService.objectMapper.readValue(json, WeatherResponse.class);
             System.out.println(weatherResponse);
