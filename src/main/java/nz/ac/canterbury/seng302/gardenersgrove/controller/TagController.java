e60d1cbc9a6b81a3344480436216cba733ba321d
Joshua Winter <jwi197@uclive.ac.nz>
2024-05-09 21:15:34 +1200

    Remove unused imports in TagController

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/TagController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/TagController.java
@@ -1,20 +1,12 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
-import nz.ac.canterbury.seng302.gardenersgrove.entity.Tag;
-import nz.ac.canterbury.seng302.gardenersgrove.location.CountryCode;
-import nz.ac.canterbury.seng302.gardenersgrove.location.map_tiler_response.SearchResult;
-import nz.ac.canterbury.seng302.gardenersgrove.service.EmailSenderService;
-import nz.ac.canterbury.seng302.gardenersgrove.service.GardenService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.TagService;
-import nz.ac.canterbury.seng302.gardenersgrove.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Called through javascript for showing autocomplete suggestions for tags.
