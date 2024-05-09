c32d0c8bc9ae9ac93bc0ef5744d301c9b4807596
Harrison Parkes <hpa101@uclive.ac.nz>
2024-05-09 17:25:50 +1200

    Made WeatherService a @Component so Spring can autowire it.

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/service/WeatherService.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/service/WeatherService.java
@@ -7,6 +7,7 @@ import nz.ac.canterbury.seng302.gardenersgrove.weather.UnableToFetchWeatherExcep
 import nz.ac.canterbury.seng302.gardenersgrove.weather.WeatherResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 import java.io.IOException;
@@ -15,6 +16,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+@Component
 public class WeatherService {
     Logger logger = LoggerFactory.getLogger(WeatherService.class);
     private final ObjectMapper objectMapper = new ObjectMapper()
@@ -38,7 +40,8 @@ public class WeatherService {
                 .GET()
                 .build();
 
-        try (HttpClient client = HttpClient.newHttpClient()) {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
             return client.send(request, HttpResponse.BodyHandlers.ofString()).body();
         } catch (IOException exception) {
             throw new UnableToFetchWeatherException("IOException occurred while getting weather data from API", exception);
