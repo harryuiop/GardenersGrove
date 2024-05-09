940f1d74153a2de2c6402d427872df2a4136bdfc
Harrison Parkes <hpa101@uclive.ac.nz>
2024-05-09 13:47:10 +1200

    moved Hourly and HourlyUnits to seperate files

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/weather/WeatherResponse.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/weather/WeatherResponse.java
@@ -1,7 +1,5 @@
 package nz.ac.canterbury.seng302.gardenersgrove.weather;
 
-import java.util.List;
-
 @java.lang.SuppressWarnings("all")
 public class WeatherResponse {
     private double latitude;
@@ -66,56 +64,3 @@ public class WeatherResponse {
     }
 }
 
-class HourlyUnits {
-    private String time;
-    private String temperature_2m;
-    private String weather_code;
-
-    @Override
-    public String toString() {
-        return "HourlyUnits{" +
-                "time='" + time + '\'' +
-                ", temperature_2m='" + temperature_2m + '\'' +
-                ", weather_code='" + weather_code + '\'' +
-                '}';
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public void setTemperature_2m(String temperature_2m) {
-        this.temperature_2m = temperature_2m;
-    }
-
-    public void setWeather_code(String weather_code) {
-        this.weather_code = weather_code;
-    }
-}
-
-class Hourly {
-    private List<String> time;
-    private List<Double> temperature_2m;
-    private List<Integer> weather_code;
-
-    @Override
-    public String toString() {
-        return "Hourly{" +
-                "time=" + time +
-                ", temperature_2m=" + temperature_2m +
-                ", weather_code=" + weather_code +
-                '}';
-    }
-
-    public void setTime(List<String> time) {
-        this.time = time;
-    }
-
-    public void setTemperature_2m(List<Double> temperature_2m) {
-        this.temperature_2m = temperature_2m;
-    }
-
-    public void setWeather_code(List<Integer> weather_code) {
-        this.weather_code = weather_code;
-    }
-}
