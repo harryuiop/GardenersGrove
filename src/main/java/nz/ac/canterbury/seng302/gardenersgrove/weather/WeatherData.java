7dbdc1dbddda9265e7d9a8835f6a58603cd17352
Vincent Chen <kch199@uclive.ac.nz>
2024-05-10 16:16:50 +1200

    implementing data process classes for weather monitoring

---
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/weather/WeatherData.java
@@ -0,0 +1,8 @@
+package nz.ac.canterbury.seng302.gardenersgrove.weather;
+
+public class WeatherData {
+    private float maximumTemperature;
+    private float minimumTemperature;
+    private int humidity;
+    private String weatherType;
+}
