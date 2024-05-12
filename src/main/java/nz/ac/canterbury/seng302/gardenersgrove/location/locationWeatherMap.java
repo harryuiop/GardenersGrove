0509e5f1fc2854394f9b75db48acc14d51db1d57
Sam Willems <swi126@uclive.ac.nz>
2024-05-12 18:36:42 +1200

    Finished adding weather code hash map to description and bootstrap icon.

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/location/locationWeatherMap.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/location/locationWeatherMap.java
@@ -22,5 +22,17 @@ public class locationWeatherMap {
         weatherCodes.put(63, new String[]{"Moderate rain", "bi bi-cloud-rain"});
         weatherCodes.put(65, new String[]{"Heavy rain", "bi bi-cloud-rain-fill"});
         weatherCodes.put(67, new String[]{"Moderate rain", "bi bi-cloud-rain-fill"});
+        weatherCodes.put(71, new String[]{"Slight snow fall", "bi bi-snow"});
+        weatherCodes.put(73, new String[]{"Moderate snow fall", "bi bi-snow3"});
+        weatherCodes.put(75, new String[]{"Heavy snow fall", "bi bi-snow2"});
+        weatherCodes.put(77, new String[]{"Snow grains", "bi bi-snow2"});
+        weatherCodes.put(80, new String[]{"Slight rain showers", "bi bi-cloud-rain"});
+        weatherCodes.put(81, new String[]{"Moderate rain showers", "bi bi-cloud-rain"});
+        weatherCodes.put(82, new String[]{"Violent rain showers", "bi bi-cloud-rain-fill"});
+        weatherCodes.put(85, new String[]{"Light snow showers", "bi bi-cloud-snow"});
+        weatherCodes.put(86, new String[]{"Heavy snow showers", "bi bi-cloud-snow-fill"});
+        weatherCodes.put(95, new String[]{"Moderate thunderstorm", "bi bi-lightning"});
+        weatherCodes.put(96, new String[]{"Thunderstorm with light hail", "bi bi-cloud-lightning-rain"});
+        weatherCodes.put(99, new String[]{"Thunderstorm with heavy hail", "bi bi-cloud-lightning-rain-fill"});
     }
 }
