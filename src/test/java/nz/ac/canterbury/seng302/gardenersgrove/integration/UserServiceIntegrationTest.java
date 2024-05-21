afa49d4bd51c1b1a29b4e0a429c863f244d97f82
Alexandra Belcher <abe150@uclive.ac.nz>
2024-05-21 14:23:30 +1200

    Updated search tests and functions to match the ACs

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/UserServiceIntegrationTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/UserServiceIntegrationTest.java
@@ -35,9 +35,9 @@ public class UserServiceIntegrationTest {
         userService = new UserService(userRepositoryMock);
         assertTrue(true);
         user1 = new User
-                ("user1@gmail.com", "Ben", "Marshal", "Password1!", "2000-01-01");
+                ("user1@gmail.com", "John", "Doe", "Password1!", "2000-01-01");
         user2 = new User
-                ("user2@gmail.com", "Marshal", "Smith,", "Password1!", "2000-01-01");
+                ("user2@gmail.com", "John", "Doe", "Password1!", "2000-01-01");
         mockRepositoryUsers = new ArrayList<>();
         mockRepositoryUsers.add(user1);
         mockRepositoryUsers.add(user2);
@@ -140,40 +140,7 @@ public class UserServiceIntegrationTest {
     }
 
     @Test
-    void getSearchedUser_searchBen_returnUser1() {
-        when(userRepositoryMock.findAll()).thenReturn(mockRepositoryUsers);
-        List<Map<String,String>> users = new ArrayList<>();
-        users.add(new HashMap<>());
-        users.getFirst().put("email", user1.getEmail());
-        users.getFirst().put("name", user1.getName());
-        Assertions.assertEquals(users, userService.getSearchedUser("Ben"));
-    }
-
-    @Test
-    void getSearchedUser_searchSmithWithRandomCasing_returnUser2() {
-        when(userRepositoryMock.findAll()).thenReturn(mockRepositoryUsers);
-        List<Map<String,String>> users = new ArrayList<>();
-        users.add(new HashMap<>());
-        users.getFirst().put("email", user2.getEmail());
-        users.getFirst().put("name", user2.getName());
-        Assertions.assertEquals(users, userService.getSearchedUser("Smith"));
-    }
-
-    @Test
-    void getSearchedUser_searchMarshal_returnBothUsers() {
-        when(userRepositoryMock.findAll()).thenReturn(mockRepositoryUsers);
-        List<Map<String,String>> users = new ArrayList<>();
-        users.add(new HashMap<>());
-        users.getFirst().put("email", user1.getEmail());
-        users.getFirst().put("name", user1.getName());
-        users.add(new HashMap<>());
-        users.getLast().put("email", user2.getEmail());
-        users.getLast().put("name", user2.getName());
-        Assertions.assertEquals(users, userService.getSearchedUser("Marshal"));
-    }
-
-    @Test
-    void getSearchedUser_searchUser1AtGmail_returnUser1() {
+    void getSearchedUser_searchU1Email_returnUser1() {
         when(userRepositoryMock.findAll()).thenReturn(mockRepositoryUsers);
         List<Map<String,String>> users = new ArrayList<>();
         users.add(new HashMap<>());
@@ -181,17 +148,19 @@ public class UserServiceIntegrationTest {
         users.getFirst().put("name", user1.getName());
         Assertions.assertEquals(users, userService.getSearchedUser("user1@gmail.com"));
     }
+
     @Test
-    void getSearchedUser_searchUser2AtGmail_returnUser1() {
+    void getSearchedUser_searchU2Email_returnUser2() {
         when(userRepositoryMock.findAll()).thenReturn(mockRepositoryUsers);
         List<Map<String,String>> users = new ArrayList<>();
         users.add(new HashMap<>());
-        users.getLast().put("email", user2.getEmail());
-        users.getLast().put("name", user2.getName());
+        users.getFirst().put("email", user2.getEmail());
+        users.getFirst().put("name", user2.getName());
         Assertions.assertEquals(users, userService.getSearchedUser("user2@gmail.com"));
     }
+
     @Test
-    void getSearchedUser_searchAtGmail_returnUser1And2() {
+    void getSearchedUser_searchJohnDoe_returnUser1And2() {
         when(userRepositoryMock.findAll()).thenReturn(mockRepositoryUsers);
         List<Map<String,String>> users = new ArrayList<>();
         users.add(new HashMap<>());
@@ -200,6 +169,13 @@ public class UserServiceIntegrationTest {
         users.add(new HashMap<>());
         users.getLast().put("email", user2.getEmail());
         users.getLast().put("name", user2.getName());
-        Assertions.assertEquals(users, userService.getSearchedUser("@gmail.com"));
+        Assertions.assertEquals(users, userService.getSearchedUser("John Doe"));
     }
+    @Test
+    void getSearchedUser_searchJohn_returnEmptyList() {
+        when(userRepositoryMock.findAll()).thenReturn(mockRepositoryUsers);
+        List<Map<String,String>> users = new ArrayList<>();
+        Assertions.assertEquals(users, userService.getSearchedUser("John"));
+    }
+
 }
