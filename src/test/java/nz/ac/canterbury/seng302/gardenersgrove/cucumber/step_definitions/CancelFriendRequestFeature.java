50d54b9bc2e361c508448ae44e00946aa146747e
hel46 <hel46@uclive.ac.nz>
2024-05-21 15:05:44 +1200

    started implementing cucumber testing

---
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/CancelFriendRequestFeature.java
@@ -0,0 +1,16 @@
+package nz.ac.canterbury.seng302.gardenersgrove.cucumber.step_definitions;
+
+import io.cucumber.java.BeforeAll;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+
+
+public class CancelFriendRequestFeature {
+
+    @BeforeAll
+    public static void before_or_after_all() {
+
+    }
+
+}
