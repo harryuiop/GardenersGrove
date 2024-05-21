24f074ba5415f24fc7b66bc011287b92db65103a
Alexandra Belcher <abe150@uclive.ac.nz>
2024-05-21 18:09:22 +1200

    Doc Strings

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/SearchResultsController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/SearchResultsController.java
@@ -20,18 +20,31 @@ import java.util.Map;
 import static nz.ac.canterbury.seng302.gardenersgrove.config.UriConfig.*;
 import static nz.ac.canterbury.seng302.gardenersgrove.config.UriConfig.sendFriendRequestUri;
 
+/**
+ * Controls the searchResults page for users. This controls what the users see when the user uses the search bar on the
+ * friends page.
+ */
 @Controller
 public class SearchResultsController {
     SearchUsersController searchUsersController;
     Logger logger = LoggerFactory.getLogger(GardenController.class);
     private final UserService userService;
 
+    /**
+     * This sets up the current user service so that the current users can be reached.
+     * @param userService the current service being used to get information about the users
+     */
     @Autowired
     public SearchResultsController(UserService userService) {
         this.userService = userService;
-        searchUsersController = new SearchUsersController(userService);
     }
 
+    /**
+     * Gets the results from the user search. Returns the display of the search results.
+     * @param searchUser The String that the user inputs to look for friends
+     * @param model      The Model object used for adding attributes to the view.
+     * @return           The searchResults page is viewable to the users
+     */
     @GetMapping(SEARCH_RESULTS_STRING)
     public String getSearchResultsPage(@RequestParam String searchUser, Model model) {
         logger.info("Get /search/result/{}", searchUser);
@@ -47,6 +60,12 @@ public class SearchResultsController {
         return "searchResults";
     }
 
+    /**
+     * Passes the search string through to the search results page
+     * @param searchUser            The string sent by the user containing the possible email or full name of the user of interest
+     * @param redirectAttributes    Allows attributes to be sent through to get as params
+     * @return  a redirect to the get page.
+     */
     @PostMapping(SEARCH_RESULTS_STRING)
     public String getSearchResults(@RequestParam String searchUser, RedirectAttributes redirectAttributes) {
         logger.info("POST /search/result/{}", searchUser);
@@ -54,6 +73,11 @@ public class SearchResultsController {
         return "redirect:"+SEARCH_RESULTS_STRING;
     }
 
+    /**
+     * Incomplete send friend request function.
+     * @param Id    The Id of the user the friend request is being sent to.
+     * @return      Goes back to the manage friends page.
+     */
     @PostMapping(SEND_FREIND_REQUEST_STRING)
     public String sendFriendRequest(@RequestParam String Id) {
         logger.info("User Id: " + Id);
