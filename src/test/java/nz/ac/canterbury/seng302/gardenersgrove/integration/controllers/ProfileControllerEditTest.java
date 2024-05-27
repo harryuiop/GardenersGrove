97b09865f2e2efceecc4163e3e5a0ea24acd99b6
Harrison Parkes <hpa101@uclive.ac.nz>
2024-05-27 15:12:39 +1200

    corrected incorrect edit profile integration test

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/controllers/ProfileControllerEditTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/controllers/ProfileControllerEditTest.java
@@ -6,6 +6,8 @@ import nz.ac.canterbury.seng302.gardenersgrove.service.UserService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,13 +18,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static nz.ac.canterbury.seng302.gardenersgrove.config.UriConfig.VIEW_PROFILE_URI_STRING;
-import static nz.ac.canterbury.seng302.gardenersgrove.config.UriConfig.editProfileUri;
+import static nz.ac.canterbury.seng302.gardenersgrove.config.UriConfig.*;
 
 @SpringBootTest
 @WithMockUser(value = "1")
 @AutoConfigureMockMvc(addFilters = false)
-public class ProfileControllerEditTest {
+class ProfileControllerEditTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -72,122 +73,19 @@ public class ProfileControllerEditTest {
         Assertions.assertEquals(newDateOfBirth, user.getDob());
     }
 
-    @Test
-    void editUserProfile_invalidEmailFormat_profileNotUpdated() throws Exception {
-        String newEmail = "janedoe2.com";
-        String newFirstName = "First";
-        String newLastName = "Last";
-        boolean noSurname = false;
-        String newDateOfBirth = "2000-05-05";
-        mockMvc.perform(MockMvcRequestBuilders.post(editProfileUri())
-                        .param("email", newEmail)
-                        .param("firstName", newFirstName)
-                        .param("lastName", newLastName)
-                        .param("noSurname", String.valueOf(noSurname))
-                        .param("dateOfBirth", newDateOfBirth))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("editProfile"));
+    @ParameterizedTest
+    @CsvSource({
+            "janedoe2.com, First, Last, false, 2000-05-05",
+            "\"\", First, Last, false, 2000-05-05",
+            "jane@doe3.com, First%#, Last, false, 2000-05-05",
+            "jane@doe4.com, \"\", Last, false, 2000-05-05",
+            "jane@doe5.com, First, Last%#, false, 2000-05-05",
+            "jane@doe6.com, First, \"\", false, 2000-05-05"
 
-        Assertions.assertNotEquals(newEmail, user.getEmail());
-        Assertions.assertNotEquals(newFirstName, user.getFirstName());
-        Assertions.assertNotEquals(newLastName, user.getLastName());
-        Assertions.assertNotEquals(newDateOfBirth, user.getDob());
-    }
-
-    @Test
-    void editUserProfile_emptyEmail_profileNotUpdated() throws Exception {
-        String newEmail = "";
-        String newFirstName = "First";
-        String newLastName = "Last";
-        boolean noSurname = false;
-        String newDateOfBirth = "2000-05-05";
-        mockMvc.perform(MockMvcRequestBuilders.post(editProfileUri())
-                        .param("email", newEmail)
-                        .param("firstName", newFirstName)
-                        .param("lastName", newLastName)
-                        .param("noSurname", String.valueOf(noSurname))
-                        .param("dateOfBirth", newDateOfBirth))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("editProfile"));
-
-        Assertions.assertNotEquals(newEmail, user.getEmail());
-        Assertions.assertNotEquals(newFirstName, user.getFirstName());
-        Assertions.assertNotEquals(newLastName, user.getLastName());
-        Assertions.assertNotEquals(newDateOfBirth, user.getDob());
-    }
-
-    @Test
-    void editUserProfile_invalidFirstNameFormat_profileNotUpdated() throws Exception {
-        String newEmail = "jane@doe3.com";
-        String newFirstName = "First%#";
-        String newLastName = "Last";
-        boolean noSurname = false;
-        String newDateOfBirth = "2000-05-05";
-        mockMvc.perform(MockMvcRequestBuilders.post(editProfileUri())
-                        .param("email", newEmail)
-                        .param("firstName", newFirstName)
-                        .param("lastName", newLastName)
-                        .param("noSurname", String.valueOf(noSurname))
-                        .param("dateOfBirth", newDateOfBirth))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("editProfile"));
-
-        Assertions.assertNotEquals(newEmail, user.getEmail());
-        Assertions.assertNotEquals(newFirstName, user.getFirstName());
-        Assertions.assertNotEquals(newLastName, user.getLastName());
-        Assertions.assertNotEquals(newDateOfBirth, user.getDob());
-    }
-    @Test
-    void editUserProfile_emptyFirstName_profileNotUpdated() throws Exception {
-        String newEmail = "jane@doe4.com";
-        String newFirstName = "";
-        String newLastName = "Last";
-        boolean noSurname = false;
-        String newDateOfBirth = "2000-05-05";
-        mockMvc.perform(MockMvcRequestBuilders.post(editProfileUri())
-                        .param("email", newEmail)
-                        .param("firstName", newFirstName)
-                        .param("lastName", newLastName)
-                        .param("noSurname", String.valueOf(noSurname))
-                        .param("dateOfBirth", newDateOfBirth))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("editProfile"));
-
-        Assertions.assertNotEquals(newEmail, user.getEmail());
-        Assertions.assertNotEquals(newFirstName, user.getFirstName());
-        Assertions.assertNotEquals(newLastName, user.getLastName());
-        Assertions.assertNotEquals(newDateOfBirth, user.getDob());
-    }
-
-    @Test
-    void editUserProfile_invalidLastNameFormat_profileNotUpdated() throws Exception {
-        String newEmail = "jane@doe5.com";
-        String newFirstName = "First";
-        String newLastName = "Last%#";
-        boolean noSurname = false;
-        String newDateOfBirth = "2000-05-05";
-        mockMvc.perform(MockMvcRequestBuilders.post(editProfileUri())
-                        .param("email", newEmail)
-                        .param("firstName", newFirstName)
-                        .param("lastName", newLastName)
-                        .param("noSurname", String.valueOf(noSurname))
-                        .param("dateOfBirth", newDateOfBirth))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("editProfile"));
-
-        Assertions.assertNotEquals(newEmail, user.getEmail());
-        Assertions.assertNotEquals(newFirstName, user.getFirstName());
-        Assertions.assertNotEquals(newLastName, user.getLastName());
-        Assertions.assertNotEquals(newDateOfBirth, user.getDob());
-    }
-
-    @Test
-    void editUserProfile_emptyLastNameNoSurnameNotChecked_profileNotUpdated() throws Exception {
-        String newEmail = "jane@doe6.com";
-        String newFirstName = "First";
-        String newLastName = "";
-        boolean noSurname = false;
-        String newDateOfBirth = "2000-05-05";
+    })
+    void editUserProfile_invalidEmailFormat_profileNotUpdated(
+            String newEmail, String newFirstName, String newLastName, boolean noSurname, String newDateOfBirth
+    ) throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post(editProfileUri())
                         .param("email", newEmail)
                         .param("firstName", newFirstName)
@@ -238,13 +136,13 @@ public class ProfileControllerEditTest {
                         .param("lastName", newLastName)
                         .param("noSurname", String.valueOf(noSurname))
                         .param("dateOfBirth", newDateOfBirth))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("editProfile"));
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl(viewProfileUri().toString()));
 
-        Assertions.assertNotEquals(newEmail, user.getEmail());
-        Assertions.assertNotEquals(newFirstName, user.getFirstName());
-        Assertions.assertNotEquals(newLastName, user.getLastName());
-        Assertions.assertNotEquals(newDateOfBirth, user.getDob());
+        Assertions.assertEquals(newEmail, user.getEmail());
+        Assertions.assertEquals(newFirstName, user.getFirstName());
+        Assertions.assertEquals(newLastName, user.getLastName());
+        Assertions.assertEquals(newDateOfBirth, user.getDob());
 
     }
 }
