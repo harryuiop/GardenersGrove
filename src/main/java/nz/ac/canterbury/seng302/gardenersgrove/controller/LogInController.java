cb92c241c0a1706a2b399867f7797c8f0b8b6e0d
Joshua Winter <jwi197@uclive.ac.nz>
2024-05-29 14:39:32 +1200

    Remove unused import

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/LogInController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/LogInController.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.util.UrlUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
