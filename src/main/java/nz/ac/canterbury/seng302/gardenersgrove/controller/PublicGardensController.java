9e0e52cf6e71a3b588559411f63b3f66902cd404
Harrison Parkes <hpa101@uclive.ac.nz>
2024-07-23 18:06:49 +1200

    Browse public garden page now updates the navbar's model

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/PublicGardensController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/PublicGardensController.java
@@ -1,6 +1,8 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
+import nz.ac.canterbury.seng302.gardenersgrove.components.NavBar;
 import nz.ac.canterbury.seng302.gardenersgrove.service.GardenService;
+import nz.ac.canterbury.seng302.gardenersgrove.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +13,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import static nz.ac.canterbury.seng302.gardenersgrove.config.UriConfig.*;
 
 @Controller
-public class PublicGardensController {
+public class PublicGardensController extends NavBar {
     Logger logger = LoggerFactory.getLogger(PublicGardensController.class);
     private final GardenService gardenService;
+    private final UserService userService;
 
     @Autowired
-    PublicGardensController(GardenService gardenService) {
+    PublicGardensController(GardenService gardenService, UserService userService) {
         this.gardenService = gardenService;
+        this.userService = userService;
     }
 
     @GetMapping(BROWSE_PUBLIC_GARDENS_URI_STRING)
@@ -25,6 +29,7 @@ public class PublicGardensController {
         logger.info("GET {}", browsePublicGardensUri());
         model.addAttribute("gardens", gardenService.getAllPublicGardens());
         model.addAttribute("viewGardenUriString", VIEW_GARDEN_URI_STRING);
+        this.updateGardensNavBar(model, gardenService, userService);
         return "publicGardens";
     }
 }
