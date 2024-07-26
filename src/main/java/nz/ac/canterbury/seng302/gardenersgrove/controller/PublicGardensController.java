6d5a25efc914309ea3596e073d7eacfd3bc76977
Harrison Parkes <hpa101@uclive.ac.nz>
2024-07-26 13:42:13 +1200

    got pagination working on the browse public gardens page by using query parameters instead of page anchor as changing anchor doesn't refresh the page

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/PublicGardensController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/PublicGardensController.java
@@ -1,6 +1,5 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import nz.ac.canterbury.seng302.gardenersgrove.components.NavBar;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
 import nz.ac.canterbury.seng302.gardenersgrove.service.GardenService;
@@ -11,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.stream.IntStream;
@@ -30,15 +30,15 @@ public class PublicGardensController extends NavBar {
     }
 
     @GetMapping(BROWSE_PUBLIC_GARDENS_URI_STRING)
-    String browseGardens(HttpServletRequest request, Model model) {
+    String browseGardens(@RequestParam(required = false) Integer page, Model model) {
         logger.info("GET {}", browsePublicGardensUri());
 
-        // Current page number is passed through the url anchor
-        String[] urlParts = request.getRequestURL().toString().split("#");
-        int currentPage = urlParts.length > 1 ? Integer.parseInt(urlParts[1]) : 1;
-        model.addAttribute("currentPage", currentPage);
+        if (page == null) {
+            page = 1;
+        }
+        model.addAttribute("currentPage", page);
 
-        List<Garden> gardenList = gardenService.getPageOfPublicGardens(currentPage);
+        List<Garden> gardenList = gardenService.getPageOfPublicGardens(page);
         model.addAttribute("gardenList", gardenList);
 
         long numberOfGardens = gardenService.countPublicGardens();
@@ -48,6 +48,7 @@ public class PublicGardensController extends NavBar {
         model.addAttribute("pageNumbers", IntStream.range(1, numberOfPages + 1).toArray());
 
         model.addAttribute("viewGardenUriString", VIEW_GARDEN_URI_STRING);
+        model.addAttribute("browsePublicGardensUriString", BROWSE_PUBLIC_GARDENS_URI_STRING);
         this.updateGardensNavBar(model, gardenService, userService);
         return "publicGardens";
     }
