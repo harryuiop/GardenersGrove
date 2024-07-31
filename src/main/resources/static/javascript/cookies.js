57f66f81eb2ba542f2fa6504f5323f01acb0097a
HanByeol Yang <hya62@uclive.ac.nz>
2024-07-31 16:33:32 +1200

    fixed input uri in cookies.js

--- src/main/resources/static/javascript/cookies.js
+++ src/main/resources/static/javascript/cookies.js
@@ -1,7 +1,10 @@
-
+// Used for sending rest requests to deployed application
+const possible_deployments = ['test', 'prod']
+const deployment = window.location.pathname.split('/')[1];
+const baseUri = deployment !== undefined && possible_deployments.includes(deployment) ?`/${deployment}` : '';
 const setRainPopCloseCookie = () => {
-    const baseURL = window.location.origin;
-    fetch(`https://${baseURL}/cookies/set-rains-popup`)
+    // console.log(baseUri);
+    fetch(`${baseUri}/cookies/set-rains-popup`)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
