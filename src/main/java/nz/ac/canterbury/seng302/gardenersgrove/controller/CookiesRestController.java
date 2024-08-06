5bb808cba0639145a4499c3b9417275e14b29cf6
HanByeol Yang <hya62@uclive.ac.nz>
2024-08-06 17:49:20 +1200

    removed unused imports

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/CookiesRestController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/CookiesRestController.java
@@ -1,16 +1,12 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import nz.ac.canterbury.seng302.gardenersgrove.service.CookiesService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
