a2eb57ca8be6572ec319480b04e9dc38ec141138
Sam Willems <swi126@uclive.ac.nz>
2024-08-07 14:52:48 +1200

    Added a scenario for AC3 and worked on its step defs.

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/EditGardenPlantsFeature.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/EditGardenPlantsFeature.java
@@ -1,23 +1,22 @@
 package nz.ac.canterbury.seng302.gardenersgrove.cucumber.step_definitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Plant;
 import nz.ac.canterbury.seng302.gardenersgrove.repository.GardenRepository;
 import nz.ac.canterbury.seng302.gardenersgrove.repository.PlantRepository;
-import org.hamcrest.Matchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static nz.ac.canterbury.seng302.gardenersgrove.config.UriConfig.*;
 import static org.hamcrest.Matchers.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
@@ -32,6 +31,7 @@ public class EditGardenPlantsFeature {
     @Autowired
     private GardenRepository gardenRepository;
 
+
     @Then("there is a list of all plants I have recorded in the garden with their name, a default image, and count and description \\(if provided).")
     public void thereIsAListOfAllPlantsIHaveRecordedInTheGardenWithTheirNameADefaultImageAndCountAndDescriptionIfProvided() throws Exception {
         Optional<Garden> garden = gardenRepository.findById(1);
@@ -42,5 +42,26 @@ public class EditGardenPlantsFeature {
         mockMvc.perform(MockMvcRequestBuilders.get(viewGardenUri(1)))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(model().attribute("plants", hasItems()));
+    }
+
+    @When("I click the edit plant button next to plant with id int {int}")
+    public void iClickTheEditPlantButtonNextToPlantWithIdInt(int plantId) throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(editPlantUri(1, plantId)))
+                .andExpect(status().isOk());
+    }
+
+
+    @Then("I am taken to the edit plant page for plant with id int {int}")
+    public void iAmTakenToTheEditPlantPageForPlantWithIdInt(int plantId) throws Exception {
+        Optional<Plant> plant = plantRepository.findById((long)plantId);
+        if (plant.isPresent()) {
+            mockMvc.perform(MockMvcRequestBuilders.get(editPlantUri(1, plantId)))
+                    .andExpect(status().isOk())
+                    .andExpect(model().attribute("plantName", plant.get().getName()));
+        }
+    }
+
+    @And("the form values are prepopulated with the details of plant with id int {int}")
+    public void theFormValuesArePrepopulatedWithTheDetailsOfPlantWithIdInt(int plantId) {
     }
 }
--- src/test/resources/nz/ac/canterbury/seng302/gardenersgrove/cucumber/u12-edit-public-garden-plants.feature
+++ src/test/resources/nz/ac/canterbury/seng302/gardenersgrove/cucumber/u12-edit-public-garden-plants.feature
@@ -2,12 +2,13 @@ Feature: As Kaia, I want to update the plants I have in my garden so that I can 
 
   Background:
     Given I have a user account that has logged in as "user1@gmail.com", "Password1!"
+    And I have a garden
     And I am on the garden details page for a garden I own
 
     Scenario: AC1 - I have a list of all plants recorded with correct details
       Then there is a list of all plants I have recorded in the garden with their name, a default image, and count and description (if provided).
 
     Scenario: AC3 - Given I click the edit plant button next to any plant, then I see the edit plant form with the plant details prepopulated.
-      When I click the edit plant button next to plant {int}
-      Then I am taken to the edit plant page for plant {int}
-      And the form values are prepopulated with the details of plant {int}
+      When I click the edit plant button next to plant with id int 1
+      Then I am taken to the edit plant page for plant with id int 1
+      And the form values are prepopulated with the details of plant with id int 1
