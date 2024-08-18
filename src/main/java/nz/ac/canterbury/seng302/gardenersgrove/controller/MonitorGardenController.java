8fa911c35afdc2ee931d55ecf21bc1ac409ef20f
Zoe Perry <zpe25@uclive.ac.nz>
2024-08-18 18:48:47 +1200

    Setting up the monitoring gardens page.

---
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/MonitorGardenController.java
@@ -0,0 +1,63 @@
+package nz.ac.canterbury.seng302.gardenersgrove.controller;
+
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.User;
+import nz.ac.canterbury.seng302.gardenersgrove.exceptions.NoSuchGardenException;
+import nz.ac.canterbury.seng302.gardenersgrove.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
+
+import static nz.ac.canterbury.seng302.gardenersgrove.config.UriConfig.*;
+
+/**
+ * Controller for the monitor garden page. For viewing statistics and live updates for a specific garden.
+ */
+@Controller
+public class MonitorGardenController {
+    private UserService userService;
+    private GardenService gardenService;
+
+    /**
+     * Spring will automatically call this constructor at runtime to inject the dependencies.
+     *
+     * @param gardenService A Garden database access object.
+     * @param userService   A User database access object.
+     */
+    @Autowired
+    public MonitorGardenController(UserService userService, GardenService gardenService) {
+        this.userService = userService;
+        this.gardenService = gardenService;
+    }
+
+    /**
+     * Set up monitor garden page and display attributes.
+     *
+     * @param gardenId The id of the garden being viewed
+     * @param model    Puts the data into the template to be viewed
+     * @return Thymeleaf html template of the monitor garden page.
+     */
+    @GetMapping(MONITOR_GARDEN_URI_STRING)
+    public String monitorGarden(
+            @PathVariable long gardenId,
+            Model model
+    ) throws NoSuchGardenException {
+        User currentUser = userService.getAuthenticatedUser();
+
+        Optional<Garden> optionalGarden = gardenService.getGardenById(gardenId);
+        if (optionalGarden.isEmpty()
+                || optionalGarden.get().getOwner().getId() != currentUser.getId()
+                && !optionalGarden.get().isGardenPublic()) {
+            throw new NoSuchGardenException(gardenId);
+        }
+        boolean owner = optionalGarden.get().getOwner() == currentUser;
+
+        model.addAttribute("garden", optionalGarden.get());
+        model.addAttribute("owner", owner);
+        model.addAttribute("connected", false); //This is where we input if the arduino is connected. Still to be implemented.
+        return "gardenMonitoring";
+    }
+
+}
