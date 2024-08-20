6c33d15ba7626fa1865cfd7efbda1a9d1342482e
Zoe Perry <zpe25@uclive.ac.nz>
2024-08-20 19:07:18 +1200

    Fixed some warnings

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/service/ArduinoDataPointService.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/service/ArduinoDataPointService.java
@@ -2,21 +2,26 @@ package nz.ac.canterbury.seng302.gardenersgrove.service;
 
 import nz.ac.canterbury.seng302.gardenersgrove.entity.ArduinoDataPoint;
 import nz.ac.canterbury.seng302.gardenersgrove.repository.ArduinoDataPointRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ArduinoDataPointService {
 
-    private ArduinoDataPointRepository dataPointRepository;
+    private final ArduinoDataPointRepository dataPointRepository;
+
+    @Autowired
+    public ArduinoDataPointService(ArduinoDataPointRepository dataPointRepository) {
+        this.dataPointRepository = dataPointRepository;
+    }
 
     /**
      * Saves a data point to the database
      *
      * @param dataPoint Arduino Data Point object to be saved
-     * @return The given data point
      */
-    public ArduinoDataPoint saveDataPoint(ArduinoDataPoint dataPoint) {
-        return dataPointRepository.save(dataPoint);
+    public void saveDataPoint(ArduinoDataPoint dataPoint) {
+        dataPointRepository.save(dataPoint);
     }
 
 }
