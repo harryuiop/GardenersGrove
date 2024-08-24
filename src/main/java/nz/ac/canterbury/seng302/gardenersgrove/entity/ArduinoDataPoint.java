562c5b9dceedee20f6c67a75eb9fd360ab980b8c
Harrison Parkes <hpa101@uclive.ac.nz>
2024-08-24 12:44:09 +1200

    Removed duplicate getter methods from ArduinoDataPoint

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/ArduinoDataPoint.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/ArduinoDataPoint.java
@@ -29,38 +29,6 @@ public class ArduinoDataPoint {
     @Column
     private double atmosphereAtm;
 
-    public Long getId() {
-        return id;
-    }
-
-    public Garden getGarden() {
-        return garden;
-    }
-
-    public LocalDateTime getTime() {
-        return time;
-    }
-
-    public double getTempCelsius() {
-        return tempCelsius;
-    }
-
-    public double getHumidityPercent() {
-        return humidityPercent;
-    }
-
-    public double getAtmosphereAtm() {
-        return atmosphereAtm;
-    }
-
-    public double getLightPercent() {
-        return lightPercent;
-    }
-
-    public double getMoisturePercent() {
-        return moisturePercent;
-    }
-
     @Column
     private double lightPercent;
 
@@ -78,7 +46,6 @@ public class ArduinoDataPoint {
     }
 
     public ArduinoDataPoint() {
-
     }
 
     public Long getId() {
@@ -112,5 +79,4 @@ public class ArduinoDataPoint {
     public double getMoisturePercent() {
         return moisturePercent;
     }
-
 }
