f4beaab566ddac0143a6dd558688d879d238e660
Joshua Winter <jwi197@uclive.ac.nz>
2024-08-26 17:45:44 +1200

    remove unused import

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/TemperatureMonitoringFeature.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/TemperatureMonitoringFeature.java
@@ -20,7 +20,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
