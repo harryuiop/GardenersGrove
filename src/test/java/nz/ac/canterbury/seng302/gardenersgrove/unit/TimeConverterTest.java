3999f870827b907b31e230efac317a6ca404ade1
Vincent Chen <kch199@uclive.ac.nz>
2024-09-10 10:25:03 +1200

    - Changed wording slightly on the monitor garden page - Implemented a utility class to make the time since last Arduino connection more readable - Started to implement tests

---
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unit/TimeConverterTest.java
@@ -0,0 +1,10 @@
+package nz.ac.canterbury.seng302.gardenersgrove.unit;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+//class TimeConverterTest {
+//    @ParameterizedTest
+//    @ValueSource(longs = {})
+//}
