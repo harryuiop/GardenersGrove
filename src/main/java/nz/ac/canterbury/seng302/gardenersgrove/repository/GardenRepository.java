73fe5465c9038ad904fdade5716ee62c927dbe3e
Joshua Winter <jwi197@uclive.ac.nz>
2024-09-11 13:36:27 +1200

    Change SQL data tables to be lower case (hopefully should fix test/prod)

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/repository/GardenRepository.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/repository/GardenRepository.java
@@ -43,10 +43,10 @@ public interface GardenRepository extends CrudRepository<Garden, Long> {
     List<Garden> findByGardenPublicTrueWithSearchParameterAndTags(int paginationOffset, String searchParameter, List<String> searchedTags);
 
     @Query(value = "SELECT DISTINCT g.* " +
-                    "FROM GARDEN g " +
-                    "LEFT JOIN PLANT p ON g.ID = p.GARDEN_ID " +
-                    "LEFT JOIN TAG_GARDENS tg ON g.ID = tg.GARDENS_ID " +
-                    "LEFT JOIN TAG t ON tg.TAGS_ID = t.ID " +
+            "FROM garden g " +
+            "LEFT JOIN plant p ON g.ID = p.GARDEN_ID " +
+            "LEFT JOIN tag_gardens tg ON g.ID = tg.GARDENS_ID " +
+            "LEFT JOIN tag t ON tg.TAGS_ID = t.ID " +
                     "WHERE g.IS_GARDEN_PUBLIC = TRUE AND (LOWER(t.name) IN (?2)) " +
                     "ORDER BY g.time_created DESC LIMIT 10 OFFSET ?1", nativeQuery = true)
     List<Garden> findByGardenPublicTrueWithTags(int paginationOffset, List<String> searchedTags);
@@ -61,10 +61,10 @@ public interface GardenRepository extends CrudRepository<Garden, Long> {
     long countByIsGardenPublicTrueWithGardenNameSearch(String searchParameter);
 
     @Query(value = "SELECT COUNT(DISTINCT g.id) " +
-                    "FROM GARDEN g " +
-                    "LEFT JOIN PLANT p ON g.ID = p.GARDEN_ID " +
-                    "LEFT JOIN TAG_GARDENS tg ON g.ID = tg.GARDENS_ID " +
-                    "LEFT JOIN TAG t ON tg.TAGS_ID = t.ID " +
+            "FROM garden g " +
+            "LEFT JOIN plant p ON g.ID = p.GARDEN_ID " +
+            "LEFT JOIN tag_gardens tg ON g.ID = tg.GARDENS_ID " +
+            "LEFT JOIN tag t ON tg.TAGS_ID = t.ID " +
                     "WHERE g.IS_GARDEN_PUBLIC = TRUE AND (LOWER(t.name) IN (?1)) ", nativeQuery = true)
     long countFindByGardenPublicTrueWithTags(List<String> searchedTags);
 
