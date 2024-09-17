be5f69fa114623e178bb8734b747080dad9cfdf2
Harrison Parkes <hpa101@uclive.ac.nz>
2024-09-18 11:47:26 +1200

    Updated ArduinoDataControllerTest to give valid atmosphere values

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/controllers/ArduinoDataControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/controllers/ArduinoDataControllerTest.java
@@ -68,11 +68,11 @@ public class ArduinoDataControllerTest {
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
 
-        String jsonString = "{\"humidityPercentage\":35,\"lightLevelPercentage\":80,\"moisturePercentage\":75,\"id\":\"testid\",\"time\":\"22-08-2024 14:36\",\"temperatureCelsius\":18,\"atmosphereAtm\":100}";
+        String jsonString = "{\"humidityPercentage\":35,\"lightLevelPercentage\":80,\"moisturePercentage\":75,\"id\":\"testid\",\"time\":\"22-08-2024 14:36\",\"temperatureCelsius\":18,\"atmosphereAtm\":1.0}";
 
         mockMvc.perform(MockMvcRequestBuilders.post(ARDUINO_SENSOR_DATA)
                         .content(jsonString)
-                        .contentType(MediaType.TEXT_PLAIN))
+                        .contentType(MediaType.APPLICATION_JSON))
                         .andExpect(status().isOk());
 
         Assertions.assertEquals(1, dataPointRepository.findAllByGardenId(gardenId).size());
@@ -86,7 +86,7 @@ public class ArduinoDataControllerTest {
     void send_invalid_data_not_saved(String jsonString) throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post(ARDUINO_SENSOR_DATA)
                         .content(jsonString)
-                        .contentType(MediaType.TEXT_PLAIN))
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
 
         Assertions.assertEquals(0, dataPointRepository.findAllByGardenId(gardenId).size());
