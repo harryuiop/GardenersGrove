e40c8f1750d6527efeb9aba78bb9d5db1f2f04e6
Zoe Perry <zpe25@uclive.ac.nz>
2024-09-19 01:04:31 +1200

    Added the dropdown for garden comparison and populated it with all the logged in users gardens.

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/MonitorGardenController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/MonitorGardenController.java
@@ -86,6 +86,7 @@ public class MonitorGardenController extends NavBar {
         model.addAttribute("garden", garden);
         model.addAttribute("owner", garden.getOwner() == currentUser);
         model.addAttribute("garden", optionalGarden.get());
+        model.addAttribute("gardenList", gardenService.getAllGardens());
 
         //This is where we input if the arduino is connected. Still to be implemented.
         model.addAttribute("connected", false);
