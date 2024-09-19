937602a94445618377b6f9e717b164810663229f
Joshua Winter <jwi197@uclive.ac.nz>
2024-09-20 10:56:34 +1200

    Ensure the minimum is the minimum and the maximum is the maximum for advice ranges. Prevent invalid characters on ranges

---
+++ src/main/resources/static/javascript/adviceRanges.js
@@ -0,0 +1,49 @@
+const adviceRanges = document.querySelectorAll(".advice-range");
+
+const minTemp = document.getElementById("minTemp");
+const maxTemp = document.getElementById("maxTemp");
+const minSoilMoisture = document.getElementById("minSoilMoisture");
+const maxSoilMoisture = document.getElementById("maxSoilMoisture");
+const minAirPressure = document.getElementById("minAirPressure");
+const maxAirPressure = document.getElementById("maxAirPressure");
+const minHumidity = document.getElementById("minHumidity");
+const maxHumidity = document.getElementById("maxHumidity");
+
+function preventInvalidCharacters(event) {
+    if (['e', 'E', '+'].includes(event.key)) {
+        event.preventDefault();
+    }
+}
+
+for (const adviceRange of adviceRanges) {
+    adviceRange.addEventListener('keydown', function(event) {
+        preventInvalidCharacters(event);
+    });
+}
+
+/**
+ * Ensure the minimum is the minimum and the maximum is the maximum for advice ranges
+ * @param minElement Input Min Element
+ * @param maxElement Input Max Element
+ */
+function ensureMinMaxCorrect(minElement, maxElement) {
+    minElement.addEventListener("input", function(event) {
+        if (event.target.value >= parseFloat(maxElement.value)) {
+            minElement.classList.add("border", "border-danger");
+        } else {
+            minElement.classList.remove("border", "border-danger");
+        }
+    });
+    maxElement.addEventListener("input", function(event) {
+        if (event.target.value <= parseFloat(maxElement.value)) {
+            maxElement.classList.add("border", "border-danger");
+        } else {
+            maxElement.classList.remove("border", "border-danger");
+        }
+    })
+}
+
+ensureMinMaxCorrect(minTemp, maxTemp);
+ensureMinMaxCorrect(minHumidity, maxHumidity);
+ensureMinMaxCorrect(minAirPressure, maxAirPressure);
+ensureMinMaxCorrect(minSoilMoisture, maxSoilMoisture);
