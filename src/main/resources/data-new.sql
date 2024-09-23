de282cc6a00abddfd2a645b2438022e1874fe00a
HanByeol Yang <hya62@uclive.ac.nz>
2024-09-23 17:33:30 +1200

    add newdata sheets to add new value into MariaDB

---
+++ src/main/resources/data-new.sql
@@ -0,0 +1,43 @@
+# This is for updating data table in our MariaDB
+# We can generate new data here and paste to data-h2.sql
+
+/*
+    UPDATE garden set advice_ranges = 1 where id = 1;
+    UPDATE garden set advice_ranges = 2 where id = 2;
+    UPDATE garden set advice_ranges = 3 where id = 3;
+    UPDATE garden set advice_ranges = 4 where id = 4;
+    UPDATE garden set advice_ranges = 5 where id = 5;
+    UPDATE garden set advice_ranges = 6 where id = 6;
+    UPDATE garden set advice_ranges = 7 where id = 7;
+    UPDATE garden set advice_ranges = 8 where id = 8;
+    UPDATE garden set advice_ranges = 9 where id = 9;
+    UPDATE garden set advice_ranges = 10 where id = 10;
+    UPDATE garden set advice_ranges = 11 where id = 11;
+    UPDATE garden set advice_ranges = 12 where id = 12;
+    UPDATE garden set advice_ranges = 13 where id = 13;
+    UPDATE garden set advice_ranges = 14 where id = 38;
+    UPDATE garden set advice_ranges = 15 where id = 39;
+    UPDATE garden set advice_ranges = 16 where id = 40;
+    UPDATE garden set advice_ranges = 17 where id = 41;
+    UPDATE garden set advice_ranges = 18 where id = 42;
+    UPDATE garden set advice_ranges = 19 where id = 43;
+    UPDATE garden set advice_ranges = 20 where id = 44;
+    UPDATE garden set advice_ranges = 21 where id = 45;
+    UPDATE garden set advice_ranges = 22 where id = 46;
+    UPDATE garden set advice_ranges = 23 where id = 47;
+    UPDATE garden set advice_ranges = 24 where id = 48;
+    UPDATE garden set advice_ranges = 25 where id = 49;
+    UPDATE garden set advice_ranges = 26 where id = 50;
+    UPDATE garden set advice_ranges = 27 where id = 51;
+    UPDATE garden set advice_ranges = 28 where id = 52;
+    UPDATE garden set advice_ranges = 29 where id = 53;
+    UPDATE garden set advice_ranges = 30 where id = 54;
+    UPDATE garden set advice_ranges = 31 where id = 55;
+    UPDATE garden set advice_ranges = 32 where id = 56;
+    UPDATE garden set advice_ranges = 33 where id = 57;
+    UPDATE garden set advice_ranges = 34 where id = 58;
+    UPDATE garden set advice_ranges = 35 where id = 59;
+    UPDATE garden set advice_ranges = 36 where id = 60;
+    UPDATE garden set advice_ranges = 37 where id = 61;
+ */
+
