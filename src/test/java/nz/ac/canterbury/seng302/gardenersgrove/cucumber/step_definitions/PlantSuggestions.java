00095262260bb908f1d045e26a9a8fa332e82118
Alexandra Belcher <abe150@uclive.ac.nz>
2024-09-26 13:21:30 +1200

    Created step defs for U40011-plant-suggestion.

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/PlantSuggestions.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/PlantSuggestions.java
@@ -3,14 +3,36 @@ package nz.ac.canterbury.seng302.gardenersgrove.cucumber.step_definitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import nz.ac.canterbury.seng302.gardenersgrove.cucumber.RunCucumberTest;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.ArduinoDataPoint;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Location;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.User;
+import nz.ac.canterbury.seng302.gardenersgrove.exceptions.ProfanityCheckingException;
 import nz.ac.canterbury.seng302.gardenersgrove.service.ArduinoDataPointService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.GardenService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.UserService;
+import nz.ac.canterbury.seng302.gardenersgrove.utility.GardenPlantSuggestions;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDateTime;
+
+import static nz.ac.canterbury.seng302.gardenersgrove.config.UriConfig.viewGardenUri;
+import static nz.ac.canterbury.seng302.gardenersgrove.utility.GardenPlantSuggestions.getSuggestions;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mockStatic;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-
 public class PlantSuggestions {
     @Autowired
     private MockMvc mockMvc;
@@ -23,16 +45,47 @@ public class PlantSuggestions {
 
     @Autowired
     private UserService userService;
-    @Given("there is 14 days worth of data from the Arduino,")
-    public void thereIsDaysWorthOfDataFromTheArduino() {
 
+    private GardenPlantSuggestions gardenPlantSuggestions;
+
+    Garden garden;
+    User user;
+    Authentication auth;
+    ResultActions result;
+
+    @Given("there is 14 days worth of data from the Arduino,")
+    public void thereIsDaysWorthOfDataFromTheArduino() throws ProfanityCheckingException {
+        gardenPlantSuggestions = new GardenPlantSuggestions(arduinoDataPointService);
+        user = new User("test@user.com", "Suggest", "Plants", "Password1!", "");
+        user.setConfirmation(true);
+        userService.addUsers(user);
+        garden = new Garden(user, "Garden", "", new Location("New Zealand", "Christchurch"), 1f, true);
+        gardenService.saveGarden(garden);
+        for (double i=10; i<100; i+=10) {
+            arduinoDataPointService.saveDataPoint(new ArduinoDataPoint(garden, LocalDateTime.now(),
+                    i-20,i-10,i/100,i-5,i-7));
+        }
+        auth = RunCucumberTest.authMaker.accept(user.getEmail(), "Password1!", userService);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        Assertions.assertEquals("Given me 3 plant suggestions given my garden has, Temperature between -10.0C-70.0C, Moisture between 3.0%-83.0%, Light between 5.0%-85.0%, Air Pressure between 0.1atm-0.9atm, Humidity between 0.0%-80.0%",
+                gardenPlantSuggestions.getArduinoPrompt(garden.getId()));
     }
 
     @When("the user clicks suggest plants on the view garden page,")
-    public void theUserClicksSuggestPlantsOnTheViewGardenPage() {
+    public void theUserClicksSuggestPlantsOnTheViewGardenPage() throws Exception {
+        try (MockedStatic<GardenPlantSuggestions> mockedSuggestions = mockStatic(GardenPlantSuggestions.class)) {
+            mockedSuggestions.when(() -> GardenPlantSuggestions.getSuggestions("Given me 3 plant suggestions given my garden has, Temperature between -10.0C-70.0C, Moisture between 3.0%-83.0%, Light between 5.0%-85.0%, Air Pressure between 0.1atm-0.9atm, Humidity between 0.0%-80.0%")).thenReturn("3 plants");
+            Assertions.assertEquals("3 plants", getSuggestions("Given me 3 plant suggestions given my garden has, Temperature between -10.0C-70.0C, Moisture between 3.0%-83.0%, Light between 5.0%-85.0%, Air Pressure between 0.1atm-0.9atm, Humidity between 0.0%-80.0%"));
+            result = mockMvc.perform(MockMvcRequestBuilders.get(viewGardenUri(garden.getId())));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        result.andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Then("data is used to suggest 3 plants that would work well in the garden.")
-    public void dataIsUsedToSuggestPlantsThatWouldWorkWellInTheGarden() {
+    public void dataIsUsedToSuggestPlantsThatWouldWorkWellInTheGarden() throws Exception {
+        result.andExpect(model().attribute("plantSuggestions", "3 plants"));
     }
 }
