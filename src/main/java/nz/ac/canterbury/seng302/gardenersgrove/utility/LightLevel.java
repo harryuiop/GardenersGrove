da747a9b49ce38e616d2f23f5cfa6df639d7d458
HanByeol Yang <hya62@uclive.ac.nz>
2024-09-27 17:12:26 +1200

    Updated LightLevel.java to get advice

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/utility/LightLevel.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/utility/LightLevel.java
@@ -4,20 +4,32 @@ package nz.ac.canterbury.seng302.gardenersgrove.utility;
  * Light level used in the advice ranges.
  */
 public enum LightLevel {
-    FULL_SHADE("Full Shade"),
-    PART_SHADE("Part Shade"),
-    PART_SUN("Part Sun"),
-    FULL_SUN("Full Sun");
+    FULL_SHADE("Full Shade", "FULL_SHADE advice", 6),
+    PART_SHADE("Part Shade", "PART_SHADE advice", 4),
+    PART_SUN("Part Sun", "PART_SUN advice", 2),
+    FULL_SUN("Full Sun", "FULL_SUN advice", 0);
 
-    public final String string;
+    private final String string;
+    private final String advice;
+    private final int minimumHour;
 
-    private LightLevel(String string) {
+    LightLevel(String string, String advice, int minimumHour) {
         this.string = string;
+        this.advice = advice;
+        this.minimumHour = minimumHour;
     }
 
     @Override
     public String toString() {
         return string;
+    }
+
+    public String getAdvice() {
+        return this.advice;
+    }
+
+    public int getMinimumHour() {
+        return this.minimumHour;
     }
 
     /**
@@ -33,5 +45,20 @@ public enum LightLevel {
             }
         }
         throw new IllegalArgumentException("Unknown light level: " + displayName);
+    }
+
+    /**
+     * Get the light level from totalHour value
+     *
+     * @param totalHour integer value that represent total hours that sensor value is more than 50 percent in 24 hours
+     * @return The light level enum the string corresponds to
+     */
+    public static LightLevel getLightLevel(int totalHour) {
+        for (LightLevel level : LightLevel.values()) {
+            if (totalHour >= level.getMinimumHour()) {
+                return level;
+            }
+        }
+        throw new IllegalArgumentException(String.format("The Hour value %d is not valid%n", totalHour));
     }
 }
